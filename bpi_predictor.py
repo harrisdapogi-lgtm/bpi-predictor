60097396196267069e28f5fc4eba551656f3d323
harrisdapogi-lgtm <harrisdapogi@gmail.com>
2025-10-24 10:39:20 +0800

    Update bpi_predictor.py

--- bpi_predictor.py
+++ bpi_predictor.py
@@ -25,7 +25,7 @@ def fetch_BDO_data(max_retries=5):
     if not api_key:
         raise ValueError("❌ ALPHAVANTAGE_API_KEY not found. Add it as a GitHub secret.")
 
-    symbol = "BDOUY"  # Bank of the Philippine Islands (PSE)
+    symbol = "PHI"  # Bank of the Philippine Islands (PSE)
     url = (
         "https://www.alphavantage.co/query?"
         f"function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}"
@@ -150,6 +150,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
